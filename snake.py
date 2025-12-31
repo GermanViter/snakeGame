092798519292a366fb73db4afed1835b72e978ae
GermanViter <german.viter@icloud.com>
2025-12-31 15:15:32 -0500

    finally made the snake fucking move

--- snake.py
+++ snake.py
@@ -18,39 +18,53 @@ RED       = (231, 71, 29)
 DARKBLUE  = (40, 70, 140)
 LIGHTBLUE = (80, 120, 200)
 
-
+DIRECTION = "RIGHT"
 
 def main():
-    global DISPLAYSURF
+    global DISPLAYSURF, DIRECTION
     pygame.init()
     DISPLAYSURF = pygame.display.set_mode((WINDOW, WINDOW))
     pygame.display.set_caption('snake')
+    x = 6
+    y = 7
 
-    snakeX = 6
-    snakeY = 7
+    snake = [(x - 3, y), (x - 2, y), (x - 1, y), (x, y)]
 
     drawBoard()
     placeFruit()
-    drawSnake(snakeX, snakeY)
-    
+    drawSnake(snake)
+
     gameStarted = False
     while True:
-        if gameStarted:
-            drawBoard()
-            snakeX += 1
-            drawSnake(snakeX, snakeY)
-
+        
         for event in pygame.event.get():
             if event.type == KEYDOWN:
-                if event.key == K_RETURN:
+                if event.key == K_UP:
+                    DIRECTION = "UP"
+                elif event.key == K_LEFT:
+                    DIRECTION = "LEFT"
+                elif event.key == K_DOWN:
+                    DIRECTION = "DOWN"
+                elif event.key == K_RIGHT:
+                    DIRECTION = "RIGHT"
+                elif event.key == K_SPACE:
                     gameStarted = True
+                    print("ENTER PRESSÉ")
 
-            if event.type == QUIT:
-               pygame.quit()
-               sys.exit()
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+
+        if gameStarted:
+            setNewHead(snake)
+            snake.pop()
+        
+
+        drawBoard()
+        drawSnake(snake)
+
         pygame.display.update()
         fpsClock.tick(FPS)
-
 
 def placeFruit():
     fruitx = random.randrange(0, WINDOW, SQUARE)
@@ -63,14 +77,23 @@ def placeFruit():
         pygame.draw.ellipse(DISPLAYSURF, RED, (fruitx, fruity, SQUARE, SQUARE))
 
 
-def drawSnake(x, y):
-    #pygame.draw.rect(DISPLAYSURF, DARKBLUE, (x, y, SQUARE * 4, SQUARE))
-    snake = [(x, y), (x + 1, y), (x + 2, y), (x + 3, y)]
+def drawSnake(snake):    
+    for (i, j) in snake:
+        pygame.draw.rect(DISPLAYSURF, DARKBLUE, (i * SQUARE, j * SQUARE, SQUARE, SQUARE))
 
-    for i in range(BOARD):
-        for j in range(BOARD):
-            if (i, j) in snake:
-                pygame.draw.rect(DISPLAYSURF, DARKBLUE, (i * SQUARE, j * SQUARE, SQUARE, SQUARE))
+
+def setNewHead(snake):
+    headX, headY = snake[0]
+    if DIRECTION == "RIGHT":
+        new_head = (headX + 1, headY)
+    elif DIRECTION == "LEFT":
+        new_head = (headX - 1, headY)
+    elif DIRECTION == "UP":
+        new_head = (headX, headY - 1)
+    elif DIRECTION == "DOWN":
+        new_head = (headX, headY + 1)
+
+    snake.insert(0, new_head)
 
 def drawBoard():
     presentColor = DARK
