327358ed57bf4b41878f4285fadd76c81ce4da09
GermanViter <german.viter@icloud.com>
2025-12-31 13:26:11 -0500

    Transform the snake into an array of boxes to simplify turning

--- snake.py
+++ snake.py
@@ -8,7 +8,8 @@ from pygame.locals import *
 WINDOW = 640
 BOARD = 16
 SQUARE = 40
-FPS = 30
+FPS = 10
+fpsClock = pygame.time.Clock()
 
 #             R    G    B
 DARK      = (162, 209, 73)
@@ -24,17 +25,31 @@ def main():
     pygame.init()
     DISPLAYSURF = pygame.display.set_mode((WINDOW, WINDOW))
     pygame.display.set_caption('snake')
-    
+
+    snakeX = 6
+    snakeY = 7
+
     drawBoard()
     placeFruit()
-    drawSnake()
+    drawSnake(snakeX, snakeY)
     
+    gameStarted = False
     while True:
+        if gameStarted:
+            drawBoard()
+            snakeX += 1
+            drawSnake(snakeX, snakeY)
+
         for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_RETURN:
+                    gameStarted = True
+
             if event.type == QUIT:
                pygame.quit()
                sys.exit()
-        pygame.display.update() 
+        pygame.display.update()
+        fpsClock.tick(FPS)
 
 
 def placeFruit():
@@ -47,21 +62,15 @@ def placeFruit():
     else:
         pygame.draw.ellipse(DISPLAYSURF, RED, (fruitx, fruity, SQUARE, SQUARE))
 
-def getSquareX():
-    posX = 6
-   
-    squareX = SQUARE * posX
-    
-    return squareX
 
-def getSquareY():
-    posY = 7
-    squareY = SQUARE * posY
+def drawSnake(x, y):
+    #pygame.draw.rect(DISPLAYSURF, DARKBLUE, (x, y, SQUARE * 4, SQUARE))
+    snake = [(x, y), (x + 1, y), (x + 2, y), (x + 3, y)]
 
-    return squareY
-
-def drawSnake():
-    pygame.draw.rect(DISPLAYSURF, DARKBLUE, (getSquareX() ,getSquareY(), SQUARE * 4, SQUARE))
+    for i in range(BOARD):
+        for j in range(BOARD):
+            if (i, j) in snake:
+                pygame.draw.rect(DISPLAYSURF, DARKBLUE, (i * SQUARE, j * SQUARE, SQUARE, SQUARE))
 
 def drawBoard():
     presentColor = DARK
